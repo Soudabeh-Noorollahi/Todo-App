db993a98259717abfa38de5e39bf0860ce53455d
Soudabeh Noorollahi <s.nourollahii@gmail.com>
2025-03-13 14:41:24 +0100

    Complete Todo page

--- components/element/RadioButton.js
+++ components/element/RadioButton.js
@@ -1,7 +1,19 @@
-import React from "react";
-
-function RadioButton() {
-  return <div>RadioButton</div>;
+function RadioButton({ status, setStatus, value, title, children }) {
+  return (
+    <div className={value}>
+      <label htmlFor={value}>
+        {children}
+        {title}
+      </label>
+      <input
+        type="radio"
+        id={value}
+        value={value}
+        checked={status === value}
+        onChange={(e) => setStatus(e.target.value)}
+      />
+    </div>
+  );
 }
 
 export default RadioButton;
--- utils/sortTodo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function sortTodo() {
-  return <div>sortTodo</div>;
-}
-
-export default sortTodo;
